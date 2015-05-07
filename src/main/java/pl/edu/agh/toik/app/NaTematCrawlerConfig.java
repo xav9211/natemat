96fc570aa5d2d8e8617d093003e5654fd0d6967c
kkrzys <klimekkrzys@gmail.com>
2015-05-07 23:32:06 +0200

    integrated project with hibernate

--- src/main/java/pl/edu/agh/toik/app/NaTematCrawlerConfig.java
+++ src/main/java/pl/edu/agh/toik/app/NaTematCrawlerConfig.java
@@ -4,10 +4,13 @@ import main.java.pl.edu.agh.toik.crawler.ICrawlerService;
 import main.java.pl.edu.agh.toik.crawler.ICrawlerSettings;
 import main.java.pl.edu.agh.toik.crawler.NaTematCrawlerService;
 import main.java.pl.edu.agh.toik.crawler.NaTematCrawlerSettings;
+import main.java.pl.edu.agh.toik.database.PersistenceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import({PersistenceConfig.class})
 @Configuration
 @ComponentScan("main.java.pl.edu.agh.toik.crawler")
 public class NaTematCrawlerConfig {
---
+++ src/main/java/pl/edu/agh/toik/database/PersistenceConfig.java
@@ -0,0 +1,68 @@
+package main.java.pl.edu.agh.toik.database;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("main.java.pl.edu.agh.toik.database.repositories")
+@PropertySource({"classpath:application.properties"})
+public class PersistenceConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getProperty("jdbc.url"));
+        dataSource.setUsername(env.getProperty("jdbc.username"));
+        dataSource.setPassword(env.getProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(env.getProperty("packagesToScan"));
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    public Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+                setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+                setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+            }
+        };
+    }
+
+}
