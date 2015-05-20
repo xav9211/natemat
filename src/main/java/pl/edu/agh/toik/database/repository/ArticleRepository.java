2a1e637fd7f7ceb5cbd9c98257759971e23c8e9d
kkrzys <klimekkrzys@gmail.com>
2015-05-20 23:46:52 +0200

    little fix in ArticleRepository

--- src/main/java/pl/edu/agh/toik/database/repository/ArticleRepository.java
+++ src/main/java/pl/edu/agh/toik/database/repository/ArticleRepository.java
@@ -3,5 +3,5 @@ package main.java.pl.edu.agh.toik.database.repository;
 import main.java.pl.edu.agh.toik.database.model.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Integer> {
+public interface ArticleRepository extends CrudRepository<Article, String> {
 }
