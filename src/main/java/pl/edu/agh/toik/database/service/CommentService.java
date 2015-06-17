2b9738749ec11c7cbf31430ba99f1fdd5a93f492
kkrzys <klimekkrzys@gmail.com>
2015-06-17 14:50:38 +0200

    added database methods to article and comment services

--- src/main/java/pl/edu/agh/toik/database/service/CommentService.java
+++ src/main/java/pl/edu/agh/toik/database/service/CommentService.java
@@ -1,11 +1,13 @@
 package main.java.pl.edu.agh.toik.database.service;
 
+import main.java.pl.edu.agh.toik.database.model.Article;
 import main.java.pl.edu.agh.toik.database.model.Comment;
 import main.java.pl.edu.agh.toik.database.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -16,6 +18,10 @@ public class CommentService {
 
     public Iterable<Comment> findAllComments() {
         return commentRepository.findAll();
+    }
+
+    public Comment findById(String id) {
+        return commentRepository.findOne(id);
     }
 
     public void saveComment(Comment comment) {
@@ -32,6 +38,39 @@ public class CommentService {
             subComment.setComment(comment);
             commentRepository.save(subComment);
         }
+    }
+
+    /**
+     *
+     * @param date for example: '2015-05-20' or '2015-05' or '2015'
+     */
+    public List<Comment> findCommentsByCreatedDate(String date) {
+        return commentRepository.findByCreatedDate(date);
+    }
+
+    /**
+     *
+     * @param date1 for example: '2015-05-20' or '2015-05' or '2015'
+     * @param date2 for example: '2015-05-20' or '2015-05' or '2015'
+     */
+    public List<Comment> findArticlesBetweenTwoCreatedDates(String date1, String date2) {
+        return commentRepository.findBetweenTwoCreatedDates(date1, date2);
+    }
+
+    public List<Comment> findCommentsForArticle(Article article) {
+        return commentRepository.findByArticle(article);
+    }
+
+    public List<Comment> findCommentsForArticleUrl(String url) {
+        return commentRepository.findByArticleUrl(url);
+    }
+
+    public List<Comment> findCommentsInLikeCounterPeriod(Integer from, Integer to) {
+        return commentRepository.findInLikeCounterPeriod(from, to);
+    }
+
+    public List<Comment> findSubCommentsForComment(Comment comment) {
+        return commentRepository.findSubCommentsByComment(comment);
     }
 
 }
